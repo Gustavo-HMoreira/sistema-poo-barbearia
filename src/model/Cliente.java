2bea1a7defe3c08bd6ca42355ce6d314492a1fcd
Gustavo-HMoreira <gustavohenriquemoreira04@gmail.com>
2025-10-06 20:25:23 -0300

    criacao classe pessoa e cliente

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -1,20 +1,17 @@
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package model;
 
-
-public class Cliente {
-    private String nome;
-    private String cpf;
-    private String endereco;
-    private String telefone;
-    private String email;
-    private int id; 
-   
-    public String getNome(){
-        return nome;
-    }
+/**
+ *
+ * @author Gustavo
+ */
+public class Cliente extends Pessoa {
     
-    public void setNome(String nome){
-        this.nome = nome;
-    }
+    private int idcliente;
+    
+    
+    
 }
