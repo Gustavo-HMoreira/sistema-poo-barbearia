072c3a2bfaed00654549ae25664210df10a0fd3e
Gustavo-HMoreira <gustavohenriquemoreira04@gmail.com>
2025-10-06 21:16:10 -0300

    adicionados conteudos de produto e atualizacoes das classes funcionario e cliente

--- src/model/Produto.java
+++ src/model/Produto.java
@@ -1,13 +1,67 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
-/**
- *
- * @author Gustavo
- */
 public class Produto {
     
+    private String nome;
+    private String descricao;
+    private String idproduto;
+    private double precounidade;
+    private int quantidadeestoque;
+    
+    
+    public Produto(String nome, String descricao, String idproduto, double precounidade, int quantidadeestoque){
+        this.nome = nome;
+        this.descricao = descricao;
+        this.idproduto = idproduto;
+        this.precounidade = precounidade;
+        this.quantidadeestoque = quantidadeestoque;
+        
+        
+        
+        }
+    
+    public String getNome(){
+            return nome;
+    }
+    
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    
+    
+    public String getDescricao(){
+        return descricao;
+    }
+    
+    public void setDescricao(String descricao){
+        this.descricao = descricao;
+    }
+    
+    
+    public String getIdProduto(){
+        return idproduto;
+    }
+    
+    public void setIdProduto(String idproduto){
+        this.idproduto = idproduto;
+    }
+    
+    
+    public double getPrecoUnidade(){
+        return precounidade;
+    }
+    
+    public void setPrecoUnidade(double precounidade){
+        this.precounidade = precounidade;
+    }
+    
+    
+    public int getQuantidadeEstoque(){
+        return quantidadeestoque;
+    }
+    
+    public void setQuantidadeEstoque(int quantidadeestoque){
+        this.quantidadeestoque = quantidadeestoque;
+    }
 }
+
