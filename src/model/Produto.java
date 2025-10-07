0239baf1552a5b50d0a0766cc51496835b264433
Gustavo-HMoreira <gustavohenriquemoreira04@gmail.com>
2025-10-06 22:37:50 -0300

    classe servico completa

--- src/model/Produto.java
+++ src/model/Produto.java
@@ -15,9 +15,7 @@ public class Produto {
         this.idproduto = idproduto;
         this.precounidade = precounidade;
         this.quantidadeestoque = quantidadeestoque;
-        
-        
-        
+                
         }
     
     public String getNome(){
--- src/model/Servico.java
+++ src/model/Servico.java
@@ -2,6 +2,63 @@ package model;
 
 public class Servico {
     
-    private
+    private String nomeservico;
+    private String descricao;
+    private int idservico;
+    private double preco;
+    private TipoServico tiposervico;
+            
+    public Servico(String nomeservico, String descricao, int idservico, double preco, TipoServico tiposervico){
+        this.nomeservico = nomeservico;
+        this.descricao = descricao;
+        this.idservico = idservico;
+        this.preco = preco;
+        this.tiposervico = tiposervico;
+    }
     
+    
+    public String getNomeServico(){
+        return nomeservico;
+    }
+    
+    public void setNomeServico(String nomeservico){
+        this.nomeservico = nomeservico;
+    }
+    
+    
+    public String getDescricao(){
+        return descricao;
+    }
+    
+    public void setDescricao(String descricao){
+        this.descricao = descricao;
+    }
+    
+    
+    public int getIdServico(){
+        return idservico;
+    }
+    
+    public void setIdServico(int idservico){
+        this.idservico = idservico;
+    }
+    
+    
+    public double getPreco(){
+        return preco;
+    }
+     
+    public void setPreco(double preco){
+        this.preco = preco;
+    }
+    
+    
+    public TipoServico getTipoServico(){
+        return tiposervico;
+    }
+    
+    public void setTipoServico(TipoServico tiposervico){
+        this.tiposervico = tiposervico;
+    }
+
 }
